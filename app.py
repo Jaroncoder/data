83515b0875ec7dd8401338e89e14caf82290d27c
Jaroncoder <144526969+Jaroncoder@users.noreply.github.com>
2024-09-25 10:16:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import os
 
 app = Flask(__name__)
 
-# Make sure to set this environment variable in Vercel or in a .env file for local use
-# Example: os.getenv("MONGO_URI")
+# MongoDB URI
 MONGO_URI = os.getenv("MONGO_URI") or "mongodb+srv://sample_user:sample123@cluster0.xig4s.mongodb.net/treasurehunt?retryWrites=true&w=majority"
 
 # Function to get MongoDB client and database
@@ -45,7 +44,8 @@ def add_user():
     # Insert user data into the collection
     users_collection.insert_one(user_data)
 
-    return jsonify({"message": "User data added successfully."})
+    # Update the success message
+    return jsonify({"message": "User created successfully!"})
 
 # Route to add path data for specific path_number
 @app.route('/add_path_data', methods=['POST'])
@@ -76,7 +76,8 @@ def add_path_data():
     # Insert the data into the collection
     collection.insert_one(path_data)
 
-    return jsonify({"message": f"Path data added to collection '{collection_name}' successfully."})
+    # Update the success message
+    return jsonify({"message": f"Path data added to '{collection_name}' successfully!"})
 
 # Route to fetch a user's question from their path collection
 @app.route('/fetch_question', methods=['POST'])
